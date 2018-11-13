05b85e96ce3a3fc3d80ac6ba2d70dfad5c2b46eb
tom miller <thomas.j.c.miller@outlook.com>
2018-11-13 16:34:06 +0000

    linted gemfile and application

--- lifestyle-app-backend/config/application.rb
+++ lifestyle-app-backend/config/application.rb
@@ -17,10 +17,9 @@ module LifestyleBackend
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :options]
+        resource '*', headers: :any, methods: %i[get post options]
       end
     end
-
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
