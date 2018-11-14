1f37aebd4d6bedb2be5f2da9f813e9eb16156f52
djstephenson7 <dan_0502@hotmail.co.uk>
2018-11-14 11:39:48 +0000

    Refactored

--- lifestyle-app-frontend/src/tests/index.test.js
+++ lifestyle-app-frontend/src/tests/index.test.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import { shallow } from './enzyme';
 
-import ToDoList from './todolist'
+import ToDoList from '../todolist'
+
+var nock = require('nock')
 
 describe('ToDoList', () => {
 
+  beforeEach(() => {
+    var getToDos = nock('http://localhost:3000')
+    .get('/to_dos/')
+    .reply(200,
+      [{ "id": 1,
+         "garden_id": null,
+         "title": "hello",
+         "body": "test",
+         "created_at": "2018-11-13T12:36:33.758Z",
+         "updated_at": "2018-11-13T12:36:33.758Z" }]
+    )
+  })
+
   it('creates todo', () => {
     const wrapper = shallow(<ToDoList />);
+    console.log(wrapper.debug())
     wrapper.find('input').simulate('change', {
       target: { value: 'hello' }
     })
