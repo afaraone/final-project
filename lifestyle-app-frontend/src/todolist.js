69852e8d69aefe154abe156abffbd717f1c9423b
Andres Faraone <andresfaraone@outlook.com>
2018-11-14 18:22:17 +0000

    Refactor add variable name to ToDo comp for clarity

--- lifestyle-app-frontend/src/todolist.js
+++ lifestyle-app-frontend/src/todolist.js
@@ -39,7 +39,7 @@ export default class ToDoList extends Component {
     .then(() => this.getToDos())
   }
 
-  addToList(data){
+  postToDo(data){
     let body = JSON.stringify({to_do: data })
 
     fetch("http://localhost:3000/to_dos/", {
@@ -50,25 +50,7 @@ export default class ToDoList extends Component {
       body: body
     }).then((response) => {return response.json()})
     .then(() => this.getToDos())
-
-    this.setState({
-      userTitle: '',
-      userBody: ''
-    })
   }
-
-  changeUserTitle(input){
-    this.setState({
-      userTitle: input
-    });
-  }
-
-  changeUserBody(input) {
-    this.setState({
-      userBody: input
-    })
-  }
-
 
   componentDidMount() {
     this.getToDos()
@@ -79,19 +61,27 @@ export default class ToDoList extends Component {
       return (
         <h1>Loading</h1>
       )
-    } else {
+    }
 
+    else {
       const todos = this.state.list.map((todo) => {
-        return( <ToDo
-          key={todo.id} data={todo}
-          completeClicked={() => this.updateToDo(todo.url)}
-          deleteClicked={() => this.deleteToDo(todo.url)}
-        />)
+        return(
+          <ToDo
+            key={todo.id} data={todo}
+            completeClicked={(url) => this.updateToDo(url)}
+            deleteClicked={(url) => this.deleteToDo(url)}
+          />
+        )
       })
+
       return(
         <div className='to-do-complete'>
-          <div className='to-do-form'><ToDoForm addToList={(data) => this.addToList(data)}/></div>
-          <div className='to-do-list'>{todos}</div>
+          <div className='to-do-form'>
+            <ToDoForm addClicked={(data) => this.postToDo(data)}/>
+          </div>
+          <div className='to-do-list'>
+            {todos}
+          </div>
         </div>
       )
     }
@@ -123,17 +113,26 @@ class ToDoForm extends Component {
     const data = {"title": this.state.title, "body": this.state.body}
     return(
       <div>
-      <input
-      onChange={ (e)=> this.changeTitle(e.target.value)}
-      value={this.state.title}
-      type="text"
-      />
-      <input
-      onChange={ (e)=> this.changeBody(e.target.value)}
-      value={this.state.body}
-      type="text"
-      />
-      <button onClick={ ()=> this.props.addToList(data)}>Submit</button>
+        <input
+          onChange={ (e)=> this.changeTitle(e.target.value)}
+          value={this.state.title}
+          type="text"
+        />
+        <input
+          onChange={ (e)=> this.changeBody(e.target.value)}
+          value={this.state.body}
+          type="text"
+        />
+
+        <button
+          onClick={ ()=> {
+            this.changeTitle('')
+            this.changeBody('')
+            this.props.addClicked(data)
+            }
+          }>
+          Submit
+        </button>
       </div>
     )
   }
@@ -141,14 +140,14 @@ class ToDoForm extends Component {
 
 class ToDo extends Component {
   render() {
-    const {title, body, complete} = this.props.data
+    const {title, body, url, complete} = this.props.data
     if (!complete){
       return(
         <div>
           <h1>{title}</h1>
           <h2>{body}</h2>
-          <button onClick={() => this.props.completeClicked()}>Complete</button>
-          <button onClick={() => this.props.deleteClicked()}>Delete</button>
+          <button onClick={() => this.props.completeClicked(url)}>Complete</button>
+          <button onClick={() => this.props.deleteClicked(url)}>Delete</button>
         </div>
       )
     } else {
