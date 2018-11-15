8b1e7facdf2e2489cbd5237239dce128d3bcf994
djstephenson7 <dan_0502@hotmail.co.uk>
2018-11-15 12:34:53 +0000

    Fixed RSpec failures

--- lifestyle-app-backend/spec/routing/to_dos_routing_spec.rb
+++ lifestyle-app-backend/spec/routing/to_dos_routing_spec.rb
@@ -5,23 +5,23 @@ require 'rails_helper'
 RSpec.describe ToDosController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
-      expect(get: '/to_dos').to route_to('to_dos#index')
+      expect(get: '/api/to_dos').to route_to('to_dos#index')
     end
 
     it 'routes to #show' do
-      expect(get: '/to_dos/1').to route_to('to_dos#show', id: '1')
+      expect(get: '/api/to_dos/1').to route_to('to_dos#show', id: '1')
     end
 
     it 'routes to #create' do
-      expect(post: '/to_dos').to route_to('to_dos#create')
+      expect(post: '/api/to_dos').to route_to('to_dos#create')
     end
 
     it 'routes to #update via PUT' do
-      expect(put: '/to_dos/1').to route_to('to_dos#update', id: '1')
+      expect(put: '/api/to_dos/1').to route_to('to_dos#update', id: '1')
     end
 
     it 'routes to #destroy' do
-      expect(delete: '/to_dos/1').to route_to('to_dos#destroy', id: '1')
+      expect(delete: '/api/to_dos/1').to route_to('to_dos#destroy', id: '1')
     end
   end
 end
