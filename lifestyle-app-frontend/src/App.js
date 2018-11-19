63de5ff5ab30b3cfc16741f20a1641ccf8ea174f
Andres Faraone <andresfaraone@outlook.com>
2018-11-19 14:20:07 +0000

    Add google login in frontend
    Returns json obj to send to backend

--- lifestyle-app-frontend/src/App.js
+++ lifestyle-app-frontend/src/App.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
 import './styles/App.css';
 import ToDoList from './todolist';
+import {GoogleLogin, GoogleLogout} from 'react-google-login';
 
 export default class App extends Component {
   render() {
+    const responseGoogle = (response) => {
+      console.log(response);
+    }
+
+    const logout = (response) => {
+      console.log(response);
+    }
+
+
     return(
+      <>
+      <GoogleLogin
+        clientId='1079599907119-tgi3sq2anv557ircj50lin80qjqs50o0'
+        buttonText='Login'
+        onSuccess={responseGoogle}
+        onFailure={responseGoogle}
+      />
+
+      <GoogleLogout
+        buttonText="Logout"
+        onLogoutSuccess={logout}
+      >
+      </GoogleLogout>
+
       <ToDoList />
+      </>
     )
   }
 }
