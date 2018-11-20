6f07819939ced7535e346b0bc1e1cc0262395f1e
Andres Faraone <andresfaraone@outlook.com>
2018-11-20 11:26:44 +0000

    fix routing tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'email' do
+    it 'gets saved if name and email present' do
+      user = User.new(name: "testname", email: "testemail")
+      expect(user.valid?).to eq true
+    end
+    it 'must be unique' do
+      User.create(name: "testname", email: "testemail")
+      user = User.new(name: "testname", email: "testemail")
+      expect(user.valid?).to eq false
+    end
+  end
 end
--- spec/routing/to_dos_routing_spec.rb
+++ spec/routing/to_dos_routing_spec.rb
@@ -5,23 +5,23 @@ require 'rails_helper'
 RSpec.describe ToDosController, type: :routing do
   describe 'routing' do
     it 'routes to #index' do
-      expect(get: '/api/to_dos').to route_to('to_dos#index')
+      expect(get: '/api/users/1/to_dos').to route_to('to_dos#index', user_id: '1')
     end
 
     it 'routes to #show' do
-      expect(get: '/api/to_dos/1').to route_to('to_dos#show', id: '1')
+      expect(get: '/api/users/1/to_dos/1').to route_to('to_dos#show', user_id: '1', id: '1')
     end
 
     it 'routes to #create' do
-      expect(post: '/api/to_dos').to route_to('to_dos#create')
+      expect(post: '/api/users/1/to_dos').to route_to('to_dos#create', user_id: '1')
     end
 
     it 'routes to #update via PUT' do
-      expect(put: '/api/to_dos/1').to route_to('to_dos#update', id: '1')
+      expect(put: '/api/users/1/to_dos/1').to route_to('to_dos#update', user_id: '1', id: '1')
     end
 
     it 'routes to #destroy' do
-      expect(delete: '/api/to_dos/1').to route_to('to_dos#destroy', id: '1')
+      expect(delete: '/api/users/1/to_dos/1').to route_to('to_dos#destroy', user_id: '1', id: '1')
     end
   end
 end
