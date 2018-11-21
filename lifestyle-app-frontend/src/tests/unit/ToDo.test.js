f63b8f8d16a7dde4ed2f77968ea5bfb97e97d740
djstephenson7 <dan_0502@hotmail.co.uk>
2018-11-21 15:41:09 +0000

    Unit tests for ToDo images

--- lifestyle-app-frontend/src/tests/unit/ToDo.test.js
+++ lifestyle-app-frontend/src/tests/unit/ToDo.test.js
@@ -3,17 +3,32 @@ import { shallow, mount } from '../../setupTests';
 
 import { SimpleToDo, TimedToDo } from '../../ToDo'
 
-let mockProps = {title: 'mockTitle', body: 'mockBody', url: 'mockUrl', complete: false}
+let mockProps = { title: 'mockTitle', body: 'mockBody', url: 'mockUrl', complete: false }
+let mockProps2 = { title: 'mockTitle', body: 'mockBody', url: 'mockUrl', complete: true }
 
 describe('SimpleToDo Unit Test', () => {
   let comp
-  beforeEach(() => {
-    comp = mount(<SimpleToDo data={mockProps} />)
-  })
 
   describe('when complete is false', () => {
+    beforeEach(() => {
+      comp = mount(<SimpleToDo data={mockProps} />)
+    })
     it('renders title', () => {
       expect(comp.contains(<h1>mockTitle</h1>)).toEqual(true)
+    })
+
+    it('renders image sprout', () => {
+      expect(comp.contains(<img className="images" src="sprout.png" alt="sprout"/>)).toEqual(true)
+    })
+  })
+
+  describe('renders a flower', () => {
+    beforeEach(() => {
+      comp = mount(<SimpleToDo data={mockProps2} />)
+    })
+
+    it('renders a flower to signify completed ToDo', () => {
+      expect(comp.contains(<img className="images" src="pink_flower.png" alt="pink_flower"/>)).toEqual(true)
     })
   })
 })
