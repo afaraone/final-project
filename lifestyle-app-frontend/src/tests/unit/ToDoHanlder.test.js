d2e5486fa6b789698f850eb3c24ce3ec36d249af
Andres Faraone <andresfaraone@outlook.com>
2018-11-22 15:59:04 +0000

    remove todohandler test for now

--- lifestyle-app-frontend/src/tests/unit/ToDoHanlder.test.js
+++
@@ -1,5 +0,0 @@
-import React, { Component } from 'react';
-import { shallow, mount } from '../../setupTests';
-import Garden from '../../Garden'
-import { FormHandler } from '../../FormHandler'
-import ToDoHandler from '../../ToDoHandler'
