ae0d853af63aec4ffead4cf62211667619cbbff2
olliebaum <oliver@baums.org.uk>
2018-11-22 17:27:20 +0000

    Initial push of non-working code

---
+++ lifestyle-app-frontend/src/todolist.js
@@ -0,0 +1,209 @@
+import React, { Component } from 'react';
+import moment from 'moment'
+import ToDoForm from './todoform'
+import TimedToDoForm from './TimedToDoForm'
+import sprout from './images/sprout.png'
+import pink_flower from './images/pink_flower.png'
+import dead from './images/dead.png'
+import FormHandler from './AddToDoButtons'
+
+
+export default class ToDoList extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      list: null,
+      isLoaded: false,
+      viewToDoForm: false
+    }
+  }
+
+  // gets all the todos from API
+  async getToDos() {
+    try {
+      let response = await fetch(this.props.url)
+      let json = await response.json()
+      this.setState({list: json, isLoaded: true})
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  // Sets a todo to complete using PUT req to API
+  async updateToDo(url) {
+    let body = JSON.stringify({"to_do": {"complete": true}})
+    try {
+      await fetch(url, {
+        method: 'PUT',
+        headers: {'Content-Type': 'application/json'},
+        body: body
+      })
+    } catch (error) {
+      console.log(error)
+    }
+    this.getToDos()
+  }
+
+  // Deletes ToDo by sending DELETE req to API
+  async deleteToDo(url) {
+    try {
+      await fetch(url, {
+        method: 'DELETE',
+        headers: {'Content-Type': 'application/json'}
+      })
+    } catch (error) {
+      console.log(error)
+    }
+    this.getToDos()
+  }
+
+  // Adds new todo by sending POST req to API
+  async postToDo(data){
+    let body = JSON.stringify({to_do: data })
+    try {
+      let response = await fetch(this.props.url, {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: body
+      })
+      let railsData = await response.json()
+      if (railsData.type !== "SimpleToDo") {
+        await this.postToCalendar(railsData)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+    this.getToDos()
+  }
+
+  async postToCalendar(data) {
+    let calendarBody = JSON.stringify({
+      "summary": data.title, "start": { "dateTime": data.start_time},
+      "end": {"dateTime": data.end_time}})
+    let response = await fetch('https://www.googleapis.com/calendar/v3/calendars/' + this.props.userDetails.email + '/events', {
+      method: 'POST',
+      headers: {'Authorization': this.props.session, 'Content-Type': 'application/json' },
+      body: calendarBody
+    })
+    let log = await response.json()
+    console.log("Beep")
+  }
+
+  // Runs automatically when component is loaded
+  componentDidMount() {
+    this.getToDos()
+  }
+
+  toggleForm() {
+    this.setState({viewToDoForm: !this.state.viewToDoForm})
+  }
+
+  render() {
+    const list = this.state.list
+    const notLoaded = !(this.state && this.state.isLoaded)
+    // If list is not loaded yet(ie before fetch is done) do the following
+    if (notLoaded) {
+      return (
+        <h1>Loading</h1>
+      )
+    }
+
+    else {
+      // Get the list and map each element onto a ToDo component
+      const todos = list.map((todo) => {
+        // Each ToDo comp is given a key, completeClicked fn and deleteClicked fn
+        if (todo.type === "SimpleToDo") {
+          return(
+            <SimpleToDo
+              key={todo.id} data={todo}
+              completeClicked={(url) => this.updateToDo(url)}
+              deleteClicked={(url) => this.deleteToDo(url)}
+            />
+          )
+        }
+        else if (todo.type  === "TimedToDo") {
+          return(
+            <TimedToDo
+              key={todo.id} data={todo}
+              completeClicked={(url) => this.updateToDo(url)}
+              deleteClicked={(url) => this.deleteToDo(url)}
+            />
+          )
+        }
+      })
+
+      return(
+        <div className='to-do-complete'>
+          {/* Load up a ToDoForm component*/}
+          <div className='add-to-do-buttons'>
+            <FormHandler postToDo={(data) => this.postToDo(data)} />
+          </div>
+          {/* Load up the group of ToDo comps made in line 76*/}
+          <div className='to-do-list'>
+            {todos}
+          </div>
+          <div className='to-do-garden'>
+            <Garden list={this.state.list}/>
+          </div>
+        </div>
+      )
+    }
+  }
+}
+
+class Garden extends Component {
+  render() {
+    const theGarden = this.props.list.map((todo) => {
+      if (todo.complete === false && new Date(todo.end_time) < new Date() && todo.end_time !== null) {
+        return(<img className='grid-item' key={todo.id} src={dead} alt='dead'/>)
+      } else if (todo.complete === false) {
+        return(<img className='grid-item' key={todo.id} src={sprout} alt='sprout'/>)
+    } else {
+        return(<img className='grid-item' key={todo.id} src={pink_flower} alt='pink_flower'/>)
+    }
+    })
+    return(
+      <div className='garden-div grid-container split right'>
+      {theGarden}
+      </div>
+    )
+  }
+}
+
+class SimpleToDo extends Component {
+  render() {
+    const {title, body, url, complete} = this.props.data
+    if (!complete){
+      return(
+        <div>
+          <h1>{title}</h1>
+          <h2>{body}</h2>
+          <button onClick={() => this.props.completeClicked(url)}>Complete</button>
+          <button onClick={() => this.props.deleteClicked(url)}>Delete</button>
+        </div>
+      )
+    } else {
+      return(null)
+    }
+  }
+}
+
+class TimedToDo extends Component {
+  render() {
+    const {title, body, url, complete, start_time, end_time} = this.props.data
+    if (!complete){
+      return(
+        <div>
+          <h1>{title}</h1>
+          <h2>{body}</h2>
+          <p>{moment(start_time).format("MMM Do YY @ h:mm a")}</p>
+          <p>{moment(end_time).format("MMM Do YY @ h:mm a")}</p>
+          <button onClick={() => this.props.completeClicked(url)}>Complete</button>
+          <button onClick={() => this.props.deleteClicked(url)}>Delete</button>
+        </div>
+      )
+    } else {
+      return(null)
+    }
+  }
+}
