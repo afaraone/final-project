fa45e6bcbcd8c8041ed9cdaac98b1912c9740f20
djstephenson7 <dan_0502@hotmail.co.uk>
2018-11-22 15:07:09 +0000

    Updated README with screenshot

---
+++ lifestyle-app-frontend/src/tests/unit/garden.test.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+import { shallow, mount } from '../../setupTests';
+
+import Garden from '../../garden';
+import { SimpleToDo, TimedToDo } from '../../ToDo.js'
+
+list = [{id: 1, type: "SimpleToDo" },
+        {id: 2, type: "TimedToDo" }
+      ]
