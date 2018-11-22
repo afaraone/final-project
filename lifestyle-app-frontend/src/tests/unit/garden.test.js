7626377af3c0dd7e6256328bb4798030c4aa0ff5
djstephenson7 <dan_0502@hotmail.co.uk>
2018-11-22 15:07:50 +0000

    Removed garden.test

--- lifestyle-app-frontend/src/tests/unit/garden.test.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import { shallow, mount } from '../../setupTests';
-
-import Garden from '../../garden';
-import { SimpleToDo, TimedToDo } from '../../ToDo.js'
-
-list = [{id: 1, type: "SimpleToDo" },
-        {id: 2, type: "TimedToDo" }
-      ]
