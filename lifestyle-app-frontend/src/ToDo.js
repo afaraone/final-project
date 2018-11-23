e15402a6caccfb22c782875c95113a44d93809b0
tom miller <thomas.j.c.miller@outlook.com>
2018-11-23 09:57:12 +0000

    calender hyperlink background changed

--- lifestyle-app-frontend/src/ToDo.js
+++ lifestyle-app-frontend/src/ToDo.js
@@ -74,7 +74,7 @@ export class TimedToDo extends Component {
           <h2>{body}</h2>
           <p>{moment(start_time).format("MMM Do YY @ h:mm a")}</p>
           <p>{moment(end_time).format("MMM Do YY @ h:mm a")}</p>
-          {details && <a href={calendar}><button><span>📅</span></button></a>}
+          {details && <a href={calendar}><span>📅</span></a>}
           {details && !complete &&
             <button className='complete-button' onClick={() => this.props.completeClicked(url)}>Complete</button>
           }
